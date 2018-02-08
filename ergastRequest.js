315f8bcbfd49537e18eea1fe576ad2cb0a5fac37
Eric <eric.ebersohl.1@gmail.com>
2018-02-07 19:42:47 -0600

    changed race info syntax

--- ergastRequest.js
+++ ergastRequest.js
@@ -13,6 +13,34 @@ let ergastRequest = {
             }).catch((err) => {
                 console.log(err);
             });
+    },
+
+    lastRace: () => {
+        return fetch('http://ergast.com/api/f1/current/last/results.json')
+            .then((res) => { return res.json(); })
+            .then((json) => {
+                let path = json['MRData']['RaceTable']['Races'][0];
+                let drivers = [];
+                let times = [];
+
+                for (var i = 0; i < json['MRData']['RaceTable']['Races'][0]['Results'].length; i++) {
+                    drivers[i] = path['Results'][i]['Driver'];
+
+                    if (path['Results'][i]['Time']) {
+                        times[i] = path['Results'][i]['Time']['time'];
+                    } else {
+                        times[i] = path['Results'][i]['status'];
+                    }
+                }
+                
+                return {
+                    raceName: path['raceName'],
+                    drivers: drivers,
+                    times: times
+                };
+            }).catch((err) => {
+                console.log(err);
+            });
     }
 };
 
